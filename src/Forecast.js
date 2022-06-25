46945e650328f470a47fb631097af8a0e3945095
scassady8 <101137225+scassady8@users.noreply.github.com>
2022-06-25 22:25:15 +0200

    Created search engine for city
    Added a search engine to search for any city

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,65 +1,41 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import FormattedDate from "./FormattedDate";
 import FormattedSunriseTime from "./FormattedSunriseTime";
 import FormattedSunsetTime from "./FormattedSunsetTime";
 import "./Forecast.css";
 
 export default function Forecast(props) {
-  const [weatherData, setWeatherData] = useState({ready: false})
-  function handleResponse(response) {
-    setWeatherData (
-      {
-        ready: true,
-        temperature: response.data.main.temp,
-        description: response.data.weather[0].description,
-        iconURL: null,
-        feelsLikeTemp: response.data.main.feels_like,
-        humidity: response.data.main.humidity,
-        wind: response.data.wind.speed,
-        date:new Date(response.data.dt * 1000),
-        sunriseUTC: new Date(response.data.sys.sunrise * 1000),
-        sunsetUTC: new Date(response.data.sys.sunset * 1000)
-      }
-    )
-  }
-  if (weatherData.ready) {  
   return (
     <div className="Forecast">
       <div className="current-weather">
-        <h1>{props.city}</h1>
+        <h1>{props.data.city}</h1>
         <div className="row">
           <div className="col-12">
-            <h4><FormattedDate date={weatherData.date} /></h4>
+            <h4><FormattedDate date={props.data.date} /></h4>
           </div>
           <div className="col-12">
-            <h4 className="text-capitalize">{weatherData.description}</h4>
+            <h4 className="text-capitalize">{props.data.description}</h4>
           </div>
           <div className="col-6 current-temp">
-            <img src={weatherData.iconURL} alt="" />
-            <span>{Math.round(weatherData.temperature)}° C</span>
+            <img src={props.data.iconURL} alt="" />
+            <span>{Math.round(props.data.temperature)}° C</span>
           </div>
           <div className="col-3 current-details">
             <ul>
-              <li>Feels like: {Math.round(weatherData.feelsLikeTemp)}° C</li>
-              <li>Humidity: {weatherData.humidity}%</li>
-              <li>Wind: {Math.round(weatherData.wind)} m/s</li>
+              <li>Feels like: {Math.round(props.data.feelsLikeTemp)}° C</li>
+              <li>Humidity: {props.data.humidity}%</li>
+              <li>Wind: {Math.round(props.data.wind)} m/s</li>
             </ul>
           </div>
           <div className="col-3 current-details">
             <ul>
-              <li><FormattedSunriseTime sunrise={weatherData.sunriseUTC}/></li>
-              <li><FormattedSunsetTime sunset={weatherData.sunsetUTC}/></li>
+              <li><FormattedSunriseTime sunrise={props.data.sunriseUTC}/></li>
+              <li><FormattedSunsetTime sunset={props.data.sunsetUTC}/></li>
             </ul>
           </div>
         </div>
       </div>
       <div className="five-day-forecast"></div>
     </div>
-  ); }
-  else {
-  const apiKey= "c6d74f51206d84d8baa8c0c74cb8a21c";
-  const apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-  }
+  ); 
 }
--- src/Search.js
+++ src/Search.js
@@ -1,19 +1,60 @@
-import React from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
+import Forecast from "./Forecast";
 import "./Search.css";
 
 export default function Search() {
+  const [weatherData, setWeatherData] = useState({ready: false})
+  const [city, setCity] = useState(props.city)
+  function handleResponse(response) {
+  setWeatherData (
+      {
+        ready: true,
+        temperature: response.data.main.temp,
+        description: response.data.weather[0].description,
+        iconURL: null,
+        feelsLikeTemp: response.data.main.feels_like,
+        humidity: response.data.main.humidity,
+        wind: response.data.wind.speed,
+        date:new Date(response.data.dt * 1000),
+        sunriseUTC: new Date(response.data.sys.sunrise * 1000),
+        sunsetUTC: new Date(response.data.sys.sunset * 1000)
+      }
+    )
+  }
+
+  function search() {
+    const apiKey= "c6d74f51206d84d8baa8c0c74cb8a21c";
+    const apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search(city); 
+    
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  if (weatherData.ready) {
   return (
     <div className="Search">
       <form>
         <div className="row search-inputs">
-          <div className="col-10">
+          <div className="col-9">
             <input
               type="search"
               className="form-control float-end input-location"
               placeholder="Enter location"
+              autoFocus="on"
+              onChange={updateCity}
+              onSubmit={handleSubmit}
             />
           </div>
-          <div className="col-2">
+          <div className="col-3 w-100">
             <input
               type="button"
               className="btn form-control float-end shadow-sm current-location-btn"
@@ -28,6 +69,11 @@ export default function Search() {
           </div>
         </div>
       </form>
+      <Forecast data={weatherData} />
     </div>
-  );
+  ); }
+  else {
+   search();
+   return "Loading...";
+  }
 }
