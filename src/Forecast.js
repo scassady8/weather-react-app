75279acc71d86c7a365b25e1631a0f4f43257fe9
scassady8 <101137225+scassady8@users.noreply.github.com>
2022-07-06 20:17:05 +0200

    Update Forecast.js

--- src/Forecast.js
+++ src/Forecast.js
@@ -17,18 +17,18 @@ export default function Forecast(props) {
           <div className="col-12">
             <h4 className="text-capitalize">{props.data.description}</h4>
           </div>
-          <div className="col-6">
+          <div className="col-4">
                 <img src={props.data.icon} alt={props.data.icondescription} />
                 <TemperatureConversion temperature={props.data.temperature} />
             </div>
-          <div className="col-3">
+          <div className="col-4">
                 <ul>
                 <li>Max: {Math.round(props.data.max)}°</li> 
                 <li>Min: {Math.round(props.data.min)}°</li> 
                 <li>Feels like: {Math.round(props.data.feelsLikeTemp)}°</li> 
                 </ul>
             </div>
-          <div className="col-3">
+          <div className="col-4">
             <ul>
               <li>Humidity: {props.data.humidity}%</li>
               <li>Wind: {Math.round(props.data.wind)} m/s</li>
