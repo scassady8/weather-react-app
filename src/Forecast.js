785d2b0032a772488867958e1f810d2d6be26258
scassady8 <101137225+scassady8@users.noreply.github.com>
2022-07-06 22:05:45 +0200

    Updated forecast display

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import FormattedDate from "./FormattedDate";
 import TemperatureConversion from './TemperatureConversion';
 import FormattedSunriseTime from "./FormattedSunriseTime";
@@ -17,23 +18,20 @@ export default function Forecast(props) {
           <div className="col-12">
             <h4 className="text-capitalize">{props.data.description}</h4>
           </div>
-          <div className="col-5">
+          <div className="col-4">
                 <img src={props.data.icon} alt={props.data.icondescription} />
                 <TemperatureConversion temperature={props.data.temperature} />
             </div>
-          <div className="col-3">
-                <ul>
-                <li>Max: {Math.round(props.data.max)}°</li> 
-                <li>Min: {Math.round(props.data.min)}°</li> 
-                <li>Feels like: {Math.round(props.data.feelsLikeTemp)}°</li> 
+          <div className="col-4">
+                <ul mt-20>
+                <li><FontAwesomeIcon icon="fa-light fa-temperature-high" /><span className="caption"> High/Low</span> {Math.round(props.data.max)}°/{Math.round(props.data.min)}°</li> 
+                <li><FontAwesomeIcon icon="fa-light fa-temperature-half" /><span className="caption"> Feels like</span> {Math.round(props.data.feelsLikeTemp)}°</li> 
                 </ul>
             </div>
           <div className="col-4">
-            <ul>
-              <li>Humidity: {props.data.humidity}%</li>
-              <li>Wind: {Math.round(props.data.wind)} m/s</li>
-              <li><FormattedSunriseTime sunrise={props.data.sunriseUTC}/></li>
-              <li><FormattedSunsetTime sunset={props.data.sunsetUTC}/></li>
+            <ul mt-20>
+              <li><FontAwesomeIcon icon="fa-light fa-droplet" /><span className="caption"> Humidity</span> {props.data.humidity}%</li>
+              <li><FontAwesomeIcon icon="fa-light fa-wind" /><span className="caption"> Wind</span> {Math.round(props.data.wind)} m/s</li>
             </ul>
           </div>
         </div>
