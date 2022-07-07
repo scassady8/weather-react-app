9a395daa812b2d3809941778b2cce2c80fa2f5b8
scassady8 <101137225+scassady8@users.noreply.github.com>
2022-07-07 21:50:48 +0200

    Updated Footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,15 +4,18 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      <p className="source-code">
+      <p className="source-code">This project was coded by {" "}<a
+          href="https://github.com/scassady8"
+          target="_blank"
+          rel="noopener noreferrer"
+        >Shannon Cassady</a>{" "} and is{" "}
         <a
           href="https://github.com/scassady8/weather-react-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Open source code
-        </a>{" "}
-        by Shannon Cassady
+          open source code
+        </a>.
       </p>
     </div>
   );
